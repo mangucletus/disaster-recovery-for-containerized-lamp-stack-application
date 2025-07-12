206c3b7957c07901565a3122f02574fbdadd95dd
mangucletus <mangucletus@gmail.com>
2025-07-12 08:04:50 +0000

    Fix ALB, RDS version, ECR, and S3 issues

--- terraform/modules/ecr/outputs.tf
+++ terraform/modules/ecr/outputs.tf
@@ -1,16 +1,14 @@
 output "repository_url" {
-  value       = aws_ecr_repository.main.repository_url
+  value       = local.repository_url
   description = "URL of the ECR repository"
 }
 
 output "repository_arn" {
-  value       = aws_ecr_repository.main.arn
+  value       = local.repository_arn
   description = "ARN of the ECR repository"
 }
 
 output "repository_name" {
-  value       = aws_ecr_repository.main.name
+  value       = local.repository_name
   description = "Name of the ECR repository"
 }
-
-
